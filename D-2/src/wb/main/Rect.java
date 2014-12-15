74916a76e16902758d7fed4a81a2a2cc8779362b
iwabuchiken <iwabuchi.k.2010@gmail.com>
2014-12-15 11:02:06 +0900

    D-3
    v-1.0-2
    ---
    1. new: views
    
    	==> created

--- D-2/src/wb/main/Rect.java
+++ D-2/src/wb/main/Rect.java
@@ -24,7 +24,13 @@ import org.eclipse.swt.widgets.Canvas;
 import org.eclipse.swt.widgets.Group;
 //import org.eclipse.wb.swt.SWTResourceManager;
 
-public class WB {
+import wb.utils.CONS;
+import org.eclipse.swt.layout.FillLayout;
+import org.eclipse.swt.layout.FormLayout;
+import org.eclipse.swt.layout.FormData;
+import org.eclipse.swt.layout.FormAttachment;
+
+public class Rect {
 
 	protected Shell shell;
 	
@@ -45,6 +51,7 @@ public class WB {
 	int count = 0;
 
 	boolean f_Executing = false;
+	private Group group_1;
 	
 	////////////////////////////////
 
@@ -59,7 +66,7 @@ public class WB {
 	public static void 
 	main(String[] args) {
 		try {
-			WB window = new WB();
+			Rect window = new Rect();
 			window.open();
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -97,9 +104,9 @@ public class WB {
 		int shell_W = (int) ((int)(float)dim.getWidth() * 0.7);
 		int shell_H = (int) ((int)(float)dim.getHeight() * 0.7);
 		
-		shell.setSize(1000, 800);
+		shell.setSize(CONS.Views.win_W, CONS.Views.win_H);
 //		shell.setSize(1200, 1000);
-		shell.setText("SWT Application");
+		shell.setText(CONS.Admin.str_MainWindow_Title);
 		
 		int shell_Loc_W = (int) (dim.getWidth() / 2 - shell.getSize().x / 2);
 		int shell_Loc_H = (int) (dim.getHeight() / 2 - shell.getSize().y / 2);
@@ -117,10 +124,47 @@ public class WB {
 
 		////////////////////////////////
 		lbl_1 = new Label(shell, SWT.NONE);
-		lbl_1.setBounds(807, 262, 161, 152);
+		FormData fd_lbl_1 = new FormData();
+		fd_lbl_1.bottom = new FormAttachment(cv_1, 0, SWT.BOTTOM);
+		fd_lbl_1.right = new FormAttachment(100, -63);
+		fd_lbl_1.top = new FormAttachment(0, 585);
+		fd_lbl_1.left = new FormAttachment(0, 854);
+		lbl_1.setLayoutData(fd_lbl_1);
 		//		lbl_1.setText("Thanks");
 				
 				lbl_1.setText("x = " + shell.getSize().x + "\n" + "y = " + shell.getSize().y);
+				
+				Group group = new Group(shell, SWT.NONE);
+				FormData fd_group = new FormData();
+				fd_group.top = new FormAttachment(group_1, 19);
+				fd_group.right = new FormAttachment(100, -39);
+				fd_group.bottom = new FormAttachment(0, 297);
+				fd_group.left = new FormAttachment(0, 824);
+				group.setLayoutData(fd_group);
+				group.setLayout(new FillLayout(SWT.HORIZONTAL));
+				
+				Button bt_Back = new Button(group, SWT.NONE);
+				bt_Back.setText("<-");
+				
+				Button bt_Forward = new Button(group, SWT.NONE);
+				bt_Forward.setText("->");
+				
+				Label lbl_Status = new Label(shell, SWT.NONE);
+				FormData fd_lbl_Status = new FormData();
+				fd_lbl_Status.bottom = new FormAttachment(group, 143, SWT.BOTTOM);
+				fd_lbl_Status.right = new FormAttachment(cv_1, 170, SWT.RIGHT);
+				fd_lbl_Status.top = new FormAttachment(group, 31);
+				fd_lbl_Status.left = new FormAttachment(cv_1, 46);
+				lbl_Status.setLayoutData(fd_lbl_Status);
+				lbl_Status.setText("Status = ");
+				
+				Label lbl_In = new Label(shell, SWT.NONE);
+				FormData fd_lbl_In = new FormData();
+				fd_lbl_In.top = new FormAttachment(group, 45);
+				fd_lbl_In.left = new FormAttachment(lbl_Status, 48);
+				fd_lbl_In.right = new FormAttachment(100, -10);
+				lbl_In.setLayoutData(fd_lbl_In);
+				lbl_In.setText("In");
 		
 	}//createContents
 
@@ -167,11 +211,11 @@ public class WB {
                 gc.dispose();
                 
                 // label
-                WB.this.f_Executing = true;
+                Rect.this.f_Executing = true;
                 
 //				while (f_Executing) {
 
-					WB.this.lbl_1.setText("executing... " + count);
+					Rect.this.lbl_1.setText("executing... " + count);
 					
 					
 //        					lbl_1.setText("clicked => " + count);
@@ -192,7 +236,7 @@ public class WB {
 			@Override
 			public void widgetSelected(SelectionEvent e) {
 				
-				WB.this.f_Executing = false;
+				Rect.this.f_Executing = false;
 //				f_Executing = false;
 				
 //				WB.this.lbl_1.setText("Cleared");
@@ -215,7 +259,7 @@ public class WB {
 //                  lp = new Point(e.x, e.y);
 //              }
 
-				WB.this.cv_1.drawBackground(gc, 0, 0, WB.this.cv_1.getSize().x, WB.this.cv_1.getSize().y);
+				Rect.this.cv_1.drawBackground(gc, 0, 0, Rect.this.cv_1.getSize().x, Rect.this.cv_1.getSize().y);
 
 				gc.dispose();
 				
@@ -228,27 +272,33 @@ public class WB {
 	private void 
 	_init_Views
 	(Shell shell) {
+		shell.setLayout(new FormLayout());
 		
 		////////////////////////////////
 
 		// buttons
 
 		////////////////////////////////
-		Group group = new Group(shell, SWT.NONE);
-		group.setBounds(804, 31, 135, 153);
+		group_1 = new Group(shell, SWT.NONE);
+		FormData fd_group_1 = new FormData();
+		fd_group_1.bottom = new FormAttachment(0, 184);
+		fd_group_1.right = new FormAttachment(0, 1002);
+		fd_group_1.top = new FormAttachment(0, 31);
+		fd_group_1.left = new FormAttachment(0, 867);
+		group_1.setLayoutData(fd_group_1);
 		
-		bt_Execute = new Button(group, SWT.NONE);
+		bt_Execute = new Button(group_1, SWT.NONE);
 		bt_Execute.setBounds(10, 10, 115, 37);
 		
 		bt_Execute.setText("Execute");
 		
-		bt_Clear = new Button(group, SWT.NONE);
+		bt_Clear = new Button(group_1, SWT.NONE);
 		bt_Clear.setBounds(10, 53, 115, 37);
 		bt_Clear.setText("Clear");
 		
 		
 		
-		bt_Quit = new Button(group, SWT.NONE);
+		bt_Quit = new Button(group_1, SWT.NONE);
 //		bt_Quit.setFont(SWTResourceManager.getFont("メイリオ", 9, SWT.ITALIC));
 //		bt_Quit.setForeground(SWTResourceManager.getColor(SWT.COLOR_BLUE));
 		
@@ -305,7 +355,12 @@ public class WB {
 
 		////////////////////////////////
 		cv_1 = new Canvas(shell, SWT.NONE);
-		cv_1.setBounds(49, 31, 711, 620);
+		FormData fd_cv_1 = new FormData();
+		fd_cv_1.bottom = new FormAttachment(0, 651);
+		fd_cv_1.right = new FormAttachment(0, 760);
+		fd_cv_1.top = new FormAttachment(0, 31);
+		fd_cv_1.left = new FormAttachment(0, 49);
+		cv_1.setLayoutData(fd_cv_1);
 		
 		Color col = display.getSystemColor(SWT.COLOR_BLUE);
 		
--- D-2/src/wb/utils/CONS.java
+++ D-2/src/wb/utils/CONS.java
@@ -2,4 +2,19 @@ package wb.utils;
 
 public class CONS {
 
+	public class Admin {
+	
+		public static final String str_MainWindow_Title = "Rectangles";
+		
+	}
+	
+	public static class Views {
+		
+		public static int win_W = 1100;
+		
+		public static int win_H = 800;
+		
+		
+		
+	}
 }
