dd1cf74856f7a6be957db91c8fdc88fbcd2b3b06
iwabuchiken <iwabuchi.k.2010@gmail.com>
2015-01-10 17:13:52 +0900

    D-12
    v-1.0#2
    ---
    1. build	=> done
    2. app	=> started

--- test_Ant/src/oata/HelloWorld.java
+++ test_Ant/src/oata/HelloWorld.java
@@ -1,11 +1,18 @@
 package oata;
 
+import org.apache.log4j.Logger;
+import org.apache.log4j.BasicConfigurator;
+
 public class HelloWorld {
 
+	static Logger logger = Logger.getLogger(HelloWorld.class);
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println("Hello World");
+//		System.out.println("Hello World");
+//		BasicConfigurator.configure();
+        logger.info("Hello World");
 		
 	}
 
